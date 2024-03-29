01388d7079f02dea5a617bbeb6e53304f79d1a17
Josefa <jmolguin@miuandes.cl>
2024-03-29 15:22:30 -0300

    add contact controller

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,11 @@
+# app/controllers/contacts_controller.rb
 class ContactController < ApplicationController
     def index
     end
-end
+  
+    def create
+      flash[:notice] = "Gracias por tu mensaje, nos pondremos en contacto contigo pronto."
+      redirect_to new_contact_path
+    end
+  end
+  
